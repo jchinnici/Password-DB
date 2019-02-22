6cfcf03262bee643b73aabbc6eab1de80f8c03aa
jchinnici <joseph.t.chinnici@gmail.com>
2019-02-21 21:29:37 -0500

    started password manager

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,15 +1,93 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
-namespace ConsoleApp3
+namespace PasswordMngr
 {
     class Program
     {
+        
+        static Dictionary<int, Record> database = new Dictionary<int, Record>();
+        [STAThread]
         static void Main(string[] args)
         {
+            Copy(1);
+            var cureentFileData = File.ReadAllText("database.txt");
+            LoadDataBase(cureentFileData);
+            while (true)
+            {
+                var input = Console.ReadLine();
+                if (input == "Add")
+                    AddRecord();
+
+                Thread.Sleep(50);
+
+            }
         }
-    }
+        static void ListRecordByName()
+        {
+            //{id} {Name}
+            //{1} {reddit}
+        }
+
+        static void Copy(int id)
+        {
+            System.Windows.Forms.Clipboard.SetText("Penis");//can only paste penis copy the id 
+        }
+        //pass what you read from file and load it in the dictionary
+        static void LoadDataBase(string text)
+        {//text.Split(",")
+            var lines = text.Split(new string[] {"\n"}, StringSplitOptions.RemoveEmptyEntries);
+            foreach(var line in lines)
+            {
+                var details = line.Split(',');
+                var newRecord = new Record();
+                newRecord.Id = Int32.Parse(details[0]);
+                newRecord.Name = details[1];
+                newRecord.Password = details[2];
+                newRecord.Tags = details[3].Split('|').ToList();
+                database.Add(newRecord.Id, newRecord);
+            }
+
+        }
+
+        private static void ToList()
+        {
+            throw new NotImplementedException();
+        }
+
+        static void AddRecord()
+        {
+            var record = new Record();
+            Console.Write("Name: ");
+            record.Name = Console.ReadLine();
+
+            Console.Write("Password: ");
+            record.Password = Console.ReadLine();
+
+            
+
+            int currentMaximumID = database.Max(x => x.Key);//takes largest id and creates new one in next number
+            record.Id = currentMaximumID + 1;
+            database.Add(record.Id, record);
+            Savedatabase();
+        }
+
+        static void RemoveRecord(int id)
+        {
+            database.Remove(id);
+        }
+
+        static void Savedatabase()
+        {
+            string allTextinDB = "";
+            foreach (var record in database)
+                allTextinDB = allTextinDB + record.Value + Environment.NewLine;//Environment.NewLine same thing as \n
+            File.WriteAllText("database.txt", allTextinDB);
+        }
+    } 
 }
---
+++ ConsoleApp3/Record.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordMngr
+{
+    class Record
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string URL { get; set; }
+        public List<string> Tags { get; set; }
+
+        public Record()
+        {
+            Tags = new List<string>();
+        }
+
+        public override string ToString()
+        {
+            return $"{Id},{Name},{Password},{string.Join("|",Tags)}";
+        }
+    }
+}
